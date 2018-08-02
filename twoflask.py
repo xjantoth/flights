eade8eff9b5f5630f44c4f1dc69b4b479c2a7272
jan.toth <jan.toth@zoomint.com>
2018-08-02 13:57:22 +0200

    fixed added json endpoints

--- twoflask.py
+++ twoflask.py
@@ -5,7 +5,7 @@ import time
 import auth
 import pytz
 import socket
-import numpy as np
+from flask import jsonify
 import datetime
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
@@ -184,33 +184,33 @@ def get_unique_days(_data):
     return final, _df
 
 
-colors = ["#000000","#FFFF00","#1CE6FF","#FF34FF","#FF4A46","#008941","#006FA6","#A30059","#FFDBE5","#7A4900",
-          "#0000A6","#63FFAC","#B79762","#004D43","#8FB0FF","#997D87","#5A0007","#809693","#FEFFE6","#1B4400",
-          "#4FC601","#3B5DFF","#4A3B53","#FF2F80","#61615A","#BA0900","#6B7900","#00C2A0","#FFAA92","#FF90C9",
-          "#B903AA","#D16100","#DDEFFF","#000035","#7B4F4B","#A1C299","#300018","#0AA6D8","#013349","#00846F",
-          "#372101","#FFB500","#C2FFED","#A079BF","#CC0744","#C0B9B2","#C2FF99","#001E09","#00489C","#6F0062",
-          "#0CBD66","#EEC3FF","#456D75","#B77B68","#7A87A1","#788D66","#885578","#FAD09F","#FF8A9A","#D157A0",
-          "#BEC459","#456648","#0086ED","#886F4C","#34362D","#B4A8BD","#00A6AA","#452C2C","#636375","#A3C8C9",
-          "#FF913F","#938A81","#575329","#00FECF","#B05B6F","#8CD0FF","#3B9700","#04F757","#C8A1A1","#1E6E00",
-          "#7900D7","#A77500","#6367A9","#A05837","#6B002C","#772600","#D790FF","#9B9700","#549E79","#FFF69F",
-          "#201625","#72418F","#BC23FF","#99ADC0","#3A2465","#922329","#5B4534","#FDE8DC","#404E55","#0089A3",
-          "#CB7E98","#A4E804","#324E72","#6A3A4C","#83AB58","#001C1E","#D1F7CE","#004B28","#C8D0F6","#A3A489",
-          "#806C66","#222800","#BF5650","#E83000","#66796D","#DA007C","#FF1A59","#8ADBB4","#1E0200","#5B4E51",
-          "#C895C5","#320033","#FF6832","#66E1D3","#CFCDAC","#D0AC94","#7ED379","#012C58","#7A7BFF","#D68E01",
-          "#353339","#78AFA1","#FEB2C6","#75797C","#837393","#943A4D","#B5F4FF","#D2DCD5","#9556BD","#6A714A",
-          "#001325","#02525F","#0AA3F7","#E98176","#DBD5DD","#5EBCD1","#3D4F44","#7E6405","#02684E","#962B75",
-          "#8D8546","#9695C5","#E773CE","#D86A78","#3E89BE","#CA834E","#518A87","#5B113C","#55813B","#E704C4",
-          "#00005F","#A97399","#4B8160","#59738A","#FF5DA7","#F7C9BF","#643127","#513A01","#6B94AA","#51A058",
-          "#A45B02","#1D1702","#E20027","#E7AB63","#4C6001","#9C6966","#64547B","#97979E","#006A66","#391406",
-          "#F4D749","#0045D2","#006C31","#DDB6D0","#7C6571","#9FB2A4","#00D891","#15A08A","#BC65E9","#FFFFFE",
-          "#C6DC99","#203B3C","#671190","#6B3A64","#F5E1FF","#FFA0F2","#CCAA35","#374527","#8BB400","#797868",
-          "#C6005A","#3B000A","#C86240","#29607C","#402334","#7D5A44","#CCB87C","#B88183","#AA5199","#B5D6C3",
-          "#A38469","#9F94F0","#A74571","#B894A6","#71BB8C","#00B433","#789EC9","#6D80BA","#953F00","#5EFF03",
-          "#E4FFFC","#1BE177","#BCB1E5","#76912F","#003109","#0060CD","#D20096","#895563","#29201D","#5B3213",
-          "#A76F42","#89412E","#1A3A2A","#494B5A","#A88C85","#F4ABAA","#A3F3AB","#00C6C8","#EA8B66","#958A9F",
-          "#BDC9D2","#9FA064","#BE4700","#658188","#83A485","#453C23","#47675D","#3A3F00","#061203","#DFFB71",
-          "#868E7E","#98D058","#6C8F7D","#D7BFC2","#3C3E6E","#D83D66","#2F5D9B","#6C5E46","#D25B88","#5B656C",
-          "#00B57F","#545C46","#866097","#365D25","#252F99","#00CCFF","#674E60","#FC009C","#92896B"]
+colors = ["#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059", "#FFDBE5", "#7A4900",
+          "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87", "#5A0007", "#809693", "#FEFFE6", "#1B4400",
+          "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80", "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9",
+          "#B903AA", "#D16100", "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+          "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09", "#00489C", "#6F0062",
+          "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66", "#885578", "#FAD09F", "#FF8A9A", "#D157A0",
+          "#BEC459", "#456648", "#0086ED", "#886F4C", "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9",
+          "#FF913F", "#938A81", "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+          "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700", "#549E79", "#FFF69F",
+          "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329", "#5B4534", "#FDE8DC", "#404E55", "#0089A3",
+          "#CB7E98", "#A4E804", "#324E72", "#6A3A4C", "#83AB58", "#001C1E", "#D1F7CE", "#004B28", "#C8D0F6", "#A3A489",
+          "#806C66", "#222800", "#BF5650", "#E83000", "#66796D", "#DA007C", "#FF1A59", "#8ADBB4", "#1E0200", "#5B4E51",
+          "#C895C5", "#320033", "#FF6832", "#66E1D3", "#CFCDAC", "#D0AC94", "#7ED379", "#012C58", "#7A7BFF", "#D68E01",
+          "#353339", "#78AFA1", "#FEB2C6", "#75797C", "#837393", "#943A4D", "#B5F4FF", "#D2DCD5", "#9556BD", "#6A714A",
+          "#001325", "#02525F", "#0AA3F7", "#E98176", "#DBD5DD", "#5EBCD1", "#3D4F44", "#7E6405", "#02684E", "#962B75",
+          "#8D8546", "#9695C5", "#E773CE", "#D86A78", "#3E89BE", "#CA834E", "#518A87", "#5B113C", "#55813B", "#E704C4",
+          "#00005F", "#A97399", "#4B8160", "#59738A", "#FF5DA7", "#F7C9BF", "#643127", "#513A01", "#6B94AA", "#51A058",
+          "#A45B02", "#1D1702", "#E20027", "#E7AB63", "#4C6001", "#9C6966", "#64547B", "#97979E", "#006A66", "#391406",
+          "#F4D749", "#0045D2", "#006C31", "#DDB6D0", "#7C6571", "#9FB2A4", "#00D891", "#15A08A", "#BC65E9", "#FFFFFE",
+          "#C6DC99", "#203B3C", "#671190", "#6B3A64", "#F5E1FF", "#FFA0F2", "#CCAA35", "#374527", "#8BB400", "#797868",
+          "#C6005A", "#3B000A", "#C86240", "#29607C", "#402334", "#7D5A44", "#CCB87C", "#B88183", "#AA5199", "#B5D6C3",
+          "#A38469", "#9F94F0", "#A74571", "#B894A6", "#71BB8C", "#00B433", "#789EC9", "#6D80BA", "#953F00", "#5EFF03",
+          "#E4FFFC", "#1BE177", "#BCB1E5", "#76912F", "#003109", "#0060CD", "#D20096", "#895563", "#29201D", "#5B3213",
+          "#A76F42", "#89412E", "#1A3A2A", "#494B5A", "#A88C85", "#F4ABAA", "#A3F3AB", "#00C6C8", "#EA8B66", "#958A9F",
+          "#BDC9D2", "#9FA064", "#BE4700", "#658188", "#83A485", "#453C23", "#47675D", "#3A3F00", "#061203", "#DFFB71",
+          "#868E7E", "#98D058", "#6C8F7D", "#D7BFC2", "#3C3E6E", "#D83D66", "#2F5D9B", "#6C5E46", "#D25B88", "#5B656C",
+          "#00B57F", "#545C46", "#866097", "#365D25", "#252F99", "#00CCFF", "#674E60", "#FC009C", "#92896B"]
 
 
 @timeit
@@ -226,7 +226,6 @@ def render_tables(_df_normalized):
     color_map = {str(u): c for u, c in zip(unique, colors)}
 
     _compare['Departure'] = pd.to_datetime(_df['std_date'] + ' ' + _df['std_time'])
-    # _compare['Departure'] = pd.to_datetime(_df['local_std_date'] + ' ' + _df['local_std_time'])
     _compare['Depart'] = _compare['Departure']
     _compare[''] = _df['route_id'].map(
         lambda x: '<span style="border-left: 12px solid {0};"></span>'.format(color_map[str(x)]))
@@ -240,8 +239,6 @@ def render_tables(_df_normalized):
     _compare['Production'] = _df['departure_iata'].map(determine_production)
     _compare['From'] = _df['departure_iata']
     _compare['To'] = _df['destination_iata']
-    # _compare['Depart UTC'] = pd.to_datetime(_df['std_date'] + ' ' + _df['std_time'])
-    # _compare['Arr UTC'] = pd.to_datetime(_df['sta_date'] + ' ' + _df['sta_time'])
     _compare['Quantity'] = _df['catering_order.quantity_y']
     _compare['Crew'] = _df['catering_order.quantity_crew']
     _compare['Extra Catering'] = _df['extra_catering'].map(extra_catering)
@@ -389,6 +386,30 @@ def create_detail_list(_compare,
 
 
 @timeit
+def create_detail_list_json(_compare,
+                            _all_unique_days,
+                            _day):
+
+    """
+
+    :param _compare:
+    :param _all_unique_days:
+    :param _day:
+    :return:
+    """
+
+    list_view = select_scoped_timeframe(_compare, _all_unique_days, _day)
+    print('list_view: {}'.format(list_view.shape[0]))
+    tmpx = list_view
+    tmpx = tmpx.groupby(['Meal', 'Direction']).count().iloc[:, 1]
+    _special_quantity = {'   '.join(k): [v, int(v) * 189] for k, v in pd.DataFrame(tmpx).to_dict()['Depart'].items()}
+    a_view = list_view.groupby(['Meal', 'Direction']).sum().to_json()
+    l_view = list_view.sort_values(['Route', 'Depart'], ascending=[True, True]).drop('Departure', axis=1).to_json()
+    print('_compare: {}'.format(_compare.shape[0]))
+    return {"special_quantity": _special_quantity, "aggregated": a_view, "list_view": l_view}
+
+
+@timeit
 def create_registration(_compare,
                         _all_unique_days,
                         _path_template,
@@ -427,6 +448,34 @@ def create_registration(_compare,
         return _data
     except Exception as e:
         pass
+
+
+@timeit
+def create_registration_json(_compare,
+                             _all_unique_days,
+                             _day,
+                             _r):
+    """
+
+    :param _compare:
+    :param _all_unique_days:
+    :param _day:
+    :param _r:
+    :return:
+    """
+
+    route_view = select_scoped_timeframe(_compare, _all_unique_days, _day)
+    _u_route = list(_compare['Route'].unique())
+    route_view = route_view.loc[route_view['Route'] == _r]
+    route_view_agg = route_view.groupby(['Meal', 'Direction']).sum().to_json()
+    route_view_list = route_view.sort_values(['Route', 'Depart'], ascending=[True, True]).drop(
+        'Departure', axis=1).to_json()
+    print({"unique_routes": _u_route,
+            "route_view_agg": route_view_agg,
+            "route_view_list": route_view_list})
+    return {"unique_routes": _u_route,
+            "route_view_agg": route_view_agg,
+            "route_view_list": route_view_list}
 
 
 win_template = auth.login["win_template"]
@@ -485,6 +534,24 @@ def get_detail_list(_day):
     return detail_data
 
 
+@app.route('/api/allud')
+def get_all_unique_days_json():
+    render, created_datetime = get_data()
+    _allud, _df_normalized = get_unique_days(render)
+    return jsonify(_allud)
+
+
+@app.route('/api/detail/<_day>')
+def get_detail_list_json(_day):
+    render, created_datetime = get_data()
+    udays, df_normalized = get_unique_days(render)
+    compare = render_tables(df_normalized)
+    detail_data_json = create_detail_list_json(compare,
+                                               udays,
+                                               _day)
+    return jsonify(detail_data_json)
+
+
 @app.route('/reg/<_day>/<_r>')
 def get_registration(_day, _r):
     render, created_datetime = get_data()
@@ -498,6 +565,19 @@ def get_registration(_day, _r):
         _day,
         _r)
     return registration_data
+
+
+@app.route('/api/reg/<_day>/<_r>')
+def get_registration_json(_day, _r):
+    render, created_datetime = get_data()
+    udays, df_normalized = get_unique_days(render)
+    compare = render_tables(df_normalized)
+    reg_data = create_registration_json(
+        compare,
+        udays,
+        _day,
+        _r)
+    return jsonify(reg_data)
 
 
 if __name__ == '__main__':
